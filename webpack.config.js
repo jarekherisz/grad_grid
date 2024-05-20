d11dd344c62b5ad65e4562af78ea5772a7435c25
Jaroslaw Herisz <jarek@herisz.pl>
2024-05-20 10:09:26 +0200

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const glob = require('glob');
+
+// Function to generate HtmlWebpackPlugin instances for each HTML file in the examples directory
+function generateHtmlPlugins(templateDir) {
+  const templateFiles = glob.sync(`${templateDir}/**/*.html`);
+  return templateFiles.map(item => {
+    const parts = item.split('/');
+    const name = parts[parts.length - 1].replace('.html', '');
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: item,
+      inject: 'body',
+    });
+  });
+}
+
+const htmlPlugins = generateHtmlPlugins('./examples');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    ...htmlPlugins
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  mode: 'development'
+};
