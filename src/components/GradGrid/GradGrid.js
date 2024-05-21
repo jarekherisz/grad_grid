8017f0be6d8de633dcf74a25490a196bc4a0b6ea
Jaroslaw Herisz <jarek@herisz.pl>
2024-05-21 12:15:17 +0200

    Update GradGrid.js

--- src/components/GradGrid/GradGrid.js
+++ src/components/GradGrid/GradGrid.js
@@ -15,22 +15,44 @@ class GradGrid extends HTMLElement {
     }
 
     connectedCallback() {
-        console.log('GradGrid connected callback');
         this.render();
 
         const mutationObserver = (mutationList, observer) => {
-            console.log('Mutation observer 11111111111');
-            for (const mutation of mutationList) {
-                if (mutation.target instanceof GradColumn) {
-                    this.initColumn(mutation.target);
-                }
-            }
+            console.log("mutationObserver start", mutationList);
+
+            mutationList.forEach(mutation => {
+                mutation.removedNodes.forEach(node => {
+                    if (node instanceof GradColumn) {
+                        this.removeColumn(node);
+                    }
+                });
+
+                mutationList.forEach(mutation => {
+                        mutation.addedNodes.forEach(node => {
+                            if (node instanceof GradColumn) {
+                                this.initColumn(node);
+                            }
+                        });
+                });
+                
+            });
         };
+
         this.mutationObserver = new MutationObserver(mutationObserver);
-        this.mutationObserver.observe(this, {childList: true, subtree: true});
+        this.mutationObserver.observe(this, {childList: true});
+
+
+        for (let i = 0; i < this.children.length; i++) {
+            if (this.children[i].tagName.toLowerCase() === 'grad-column') {
+                this.children[i].addEventListener('init-column', (event) => {
+                    this.initColumn(event.detail.column);
+                });
+            }
+        }
     }
 
     disconnectedCallback() {
+        console.log('GradGrid disconnected callback');
         this.mutationObserver.disconnect();
     }
 
@@ -90,20 +112,41 @@ class GradGrid extends HTMLElement {
             if (this.getColumnById(column.id) === undefined) {
                 this.columns.push(column);
 
+                column.addEventListener('column-updated', this.updateColumn);
+
                 let th = column.renderHeaderCell();
                 this.theadRow.appendChild(th);
             }
         }
     }
 
+    removeColumn(columnToRemove) {
+        // Sprawdzanie, czy podana kolumna znajduje się w this.columns
+        const index = this.columns.indexOf(columnToRemove);
+        if (index !== -1) {
+            // Usuwanie kolumny z listy this.columns
+            this.columns.splice(index, 1);
+
+            // Usuwanie odpowiadającego elementu <th> z <thead>
+            // Zakładając, że kolumna ma właściwość `id`, która jest używana do identyfikacji elementu <th>
+            const th = this.theadRow.querySelector(`th[data-id="${columnToRemove.id}"]`);
+            if (th) {
+                this.theadRow.removeChild(th);
+            }
+        }
+    }
+
 
     /**
-     * @param {GradColumn} column - Obiekt kolumny.
+     * @param event
      * @returns {void}
      */
-    updateColumn(column) {
-        console.log('Update columns');
+    updateColumn(event) {
+        const { column, attributeName, oldValue, newValue } = event.detail;
+
+        column.renderHeaderCell();
     }
+
 }
 
 export default GradGrid;
